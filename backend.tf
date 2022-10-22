4866b0587791413cd122b7ff15149fb31da80a05
mohab <mohab5897@gmail.com>
2022-10-22 14:47:27 +0200

    1st commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "heterrformbuck"
+    bucket         = "terrformbuck"
     dynamodb_table = "terraform-state-lock-dynamo"
     key            = "terraform.tfstate"
     region         = "us-east-1"
