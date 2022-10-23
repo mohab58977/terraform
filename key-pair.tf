22030dfcfc37d296f63fa9b140f6053664661a6e
mohab <mohab5897@gmail.com>
2022-10-23 12:22:20 +0200

    added_keypair

--- key-pair.tf
+++ key-pair.tf
@@ -1,12 +1,15 @@
 resource "aws_key_pair" "key_pair" {
-  key_name   = "key_pair"
-  public_key = tls_private_key.rsa-key.public_key_openssh
+  key_name   = "key_pair"       # Create a "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
   tags = {
     Name = "terraform_key_pair"
   }
+  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./Key_pair.pem"
+  }
 }
 
-resource "tls_private_key" "rsa-key" {
+resource "tls_private_key" "pk" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
